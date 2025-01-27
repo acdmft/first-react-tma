2d54ea899f6cc5ef5fd77eb38ff0181bebdc8883
gilert <crankyfox@protonmail.com>
2025-01-27 21:38:57 +0100

    modify postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
