6a9b2be4b40ed146569f772d11583db25fc6f574
gilert <crankyfox@protonmail.com>
2025-01-27 15:56:43 +0100

    add backButton

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 // import { useState, useEffect } from 'react'; 
 // import { useLaunchParams } from '@telegram-apps/sdk-react';
-
+import {  backButton } from '@telegram-apps/sdk-react';
 function App() {
   const groupId = 'fake group id'
   const windowInfo = window.location.href;
-
+  backButton.mount();
   return (
     <>
        <main className="flex flex-col gap-8">
